f6ab857064e7e81286f722e6e8246f4f800a2a6e
jake <jake.ejrichardson@gmail.com>
2021-06-10 20:32:30 -0400

    Create gameGrid.svelte

---
+++ src/gameGrid.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import type {Game} from "./types/Game.type"
+    // export let game : Game
+
+
+</script>
+
+<div class="game-container">
+    <img class="box-art" src="images/persona4.jpg" alt="" />
+    <h3>Persona 4 Golden</h3>
+
+</div>
+
+
+<style>
+
+</style>
