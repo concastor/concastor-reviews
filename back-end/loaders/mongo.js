ae635f5ef7f1546a7bc3705c63aa4b15ab6f82a0
jake <jake.ejrichardson@gmail.com>
2021-09-09 20:07:24 -0400

    update mongo inforamtino

--- back-end/loaders/mongo.js
+++ back-end/loaders/mongo.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require("mongodb")
 
-const uri = process.env.MONGO_CONNECTION
+const MONGO_URL = process.env.MONGO_CONNECTION
 
 let database
 
@@ -9,7 +9,7 @@ async function initDB() {
 	//checking if database is already connected
 	if (typeof database === "undefined") {
 		try {
-			const client = new MongoClient(uri, {
+			const client = new MongoClient(MONGO_URL, {
 				useNewUrlParser: true,
 				useUnifiedTopology: true,
 			})
