2347fcd3d397d579d08e27bdffa4eae15167cbee
jake <jake.ejrichardson@gmail.com>
2022-05-14 10:07:32 -0400

    add routes and pages

--- Angular-Frontend/src/app/app-routing.module.ts
+++ Angular-Frontend/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllPageComponent } from './components/all-page/all-page.component';
+import { CreatePageComponent } from './components/create-page/create-page.component';
+import { GamePageComponent } from './components/game-page/game-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Review/:title', component: GamePageComponent },
+  { path: '/home', component: HomePageComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+  { path: '/all', component: AllPageComponent },
+  { path: '/create', component: CreatePageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- Angular-Frontend/src/app/app.module.ts
+++ Angular-Frontend/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { GamePageComponent } from './components/game-page/game-page.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { CreatePageComponent } from './components/create-page/create-page.component';
+import { AllPageComponent } from './components/all-page/all-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomePageComponent,
+    GamePageComponent,
+    PageNotFoundComponent,
+    CreatePageComponent,
+    AllPageComponent
   ],
   imports: [
     BrowserModule,
