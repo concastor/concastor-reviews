94f488d0f4268f3fe231f9438577d11ad82cb1d1
jake <jake.ejrichardson@gmail.com>
2022-08-21 21:28:05 -0400

    update file names and add production npm modules

--- back-end/app.js
+++ back-end/app.js
@@ -15,6 +15,8 @@ var app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(compression()); //Compress all routes
+app.use(helmet());
 
 var port = process.env.PORT || 4000;
 var router = express.Router();
